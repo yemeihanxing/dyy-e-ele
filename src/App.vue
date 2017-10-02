3b04cb81f2f0eb36edb7c7a2ce6d166d40073932
dyy <137706578@qq.com>
2017-10-02 15:36:20 +0800

    初始化vue，搭建了主界面的头部UI

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  import header from './components/header/header';
+export default {
+  name: 'app',
+  data(){
+    return {
+      seller:{}
+    }
+  },
+  created(){
+    const Super = this;
+    axios.get('/api/seller').then(function (response) {
+      Super.seller = response.data.data;
+      console.log('response:',response.data.data);
+    }).catch((error)=>{
+      console.log('error:',error);
+    })
+  },
+  components:{
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
